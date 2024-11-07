7386fa2364eedb4269b963dffc0d943dc2d22165
Joao Paulo <joaopaulloss477@gmail.com>
2024-11-07 18:57:07 -0300

    Estudas Js e Python

--- JavaScript/Objetos/script.js
+++ JavaScript/Objetos/script.js
@@ -106,8 +106,58 @@
     // Change Age in both
     x.age = 10;
 
+    let x = "firstname";
+    let y = "age";
+    person[x] + " is " + person[y] + " years old.";
+
 /*
       O objeto x não é uma cópia de pessoa. O objeto x é pessoa.
       O objeto x e o objeto person compartilham o mesmo endereço de memória.
       Quaisquer alterações em x também mudarão de pessoa
 */
+//______________________________________________________________________________
+
+//  --- Adicionando novas propriedades ---
+
+  person.nationality = "English";
+
+//______________________________________________________________________________
+
+//  --- Excluindo propriedades ---
+
+  const person4 = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 50,
+    eyeColor: "blue"
+  };
+
+  delete person4.age;
+
+//______________________________________________________________________________
+
+//  --- Objetos aninhados ---
+
+    myObj = {
+      name:"John",
+      age:30,
+      myCars: {
+        car1:"Ford",
+        car2:"BMW",
+        car3:"Fiat"
+      }
+    }
+
+  // Você pode acessar objetos aninhados usando a notação de ponto ou a notação de colchetes:
+
+  myObj.myCars.car2; // Possibilidade 001
+
+  myObj.myCars["car2"]; // Possibilidade 002
+
+  myObj["myCars"]["car2"]; // Possibilidade 003
+
+  let p1 = "myCars";
+  let p2 = "car2"; 
+  myObj[p1][p2]; // Possibilidade 004
+
+//______________________________________________________________________________
